2712260b75b4df456807d33f5bc4704b6831bec2
esouthwood <uqz19ghu@uea.ac.uk>
2023-12-02 23:11:13 +0000

    Created Add new bird page
    Created the page for adding new bird

---
+++ main2.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
+
+
+function filterTwo(){
+  var divsToHide = document.getElementsByClassName("three"); //divsToHide is an array
+    for(var i = 0; i < divsToHide.length; i++){
+        divsToHide[i].style.visibility = "hidden"; // or
+        divsToHide[i].style.display = "none"; // depending on what you're doing
+    }
+}
+function filterOne(){
+  var divsToHide = document.getElementsByClassName("two"); //divsToHide is an array
+    for(var i = 0; i < divsToHide.length; i++){
+        divsToHide[i].style.visibility = "hidden"; // or
+        divsToHide[i].style.display = "none"; // depending on what you're doing
+    }
+}
+
+function filterThree(){
+  var divsToHide = document.getElementsByClassName("one"); //divsToHide is an array
+    for(var i = 0; i < divsToHide.length; i++){
+        divsToHide[i].style.visibility = "hidden"; // or
+        divsToHide[i].style.display = "none"; // depending on what you're doing
+    }
+}
