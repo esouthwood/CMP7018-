de9769c78279fe6ec3f7b2b4663cc8a1e55513e3
Evavs <98756351+Evavs@users.noreply.github.com>
2023-12-08 21:13:33 +0000

    new

--- main.js
+++ main.js
@@ -17,3 +17,29 @@ function incDec(e) {
   val = val < 0 ? 0 : val;
   num.value = val;
 }
+
+function customizeText(size) {
+  var elements = document.querySelectorAll(".changeable-text");
+
+  elements.forEach(function(element) {
+    // Change font size
+    var currentSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
+    //var newSize = (parseFloat(currentSize) + 2) + "px"; // Increase by 2 pixels
+    element.style.fontSize = size;
+
+    // Change color
+    var newColor = "red"; // Call a function to get a random color
+    element.style.color = newColor;
+  });
+}
+
+function applyColorMode() {
+  var body = document.body;
+  var colorMode = document.getElementById("colorMode").value;
+
+  if (colorMode === "colorBlind") {
+    body.classList.add("color-blind");
+  } else {
+    body.classList.remove("color-blind");
+  }
+}
